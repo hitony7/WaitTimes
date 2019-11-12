6e0bae5e8ba174431ee71f60c7d034b0dec83981
Russell McWhae <russell.mcwhae@gmail.com>
2019-11-12 15:37:44 -0700

    Adds basic admin component.

---
+++ client/src/Admin.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Form, Icon, Input, Button } from 'antd';
+import { Redirect } from "react-router-dom";
+// import $ from 'jquery';
+import axios from 'axios';
+import NOT_LOGGED_IN from './App.js';
+
+class Admin extends Component {
+
+  state = {
+    redirect: false,
+    message: 'Welcome to VisitER! Click the button below to load data!',
+  }
+
+  componentDidUpdate() {
+    if (this.state.redirect) {
+      this.setState({ redirect: false })
+    }
+  }
+
+  fetchData = () => {
+    axios
+      .get('/api/data') // You can simply make your requests to "/api/whatever you want"
+      .then(response => {
+        // handle success
+        // console.log(response.data); // The entire response from the Rails API
+        // console.log(response.data.message); // Just the message
+        this.setState({
+          message: response.data.message
+        });
+      });
+  };
+
+  render() {
+    // const { getFieldDecorator } = this.props.form;
+    if (this.state.redirect) {
+      return (<Redirect to='/admin' />)
+    }
+    if (this.state.loggedInStatus === NOT_LOGGED_IN) {
+      return (<Redirect to='/' />)
+    }
+    return (
+      <div>
+        <h2>{this.state.message}</h2>
+        <Button onClick={this.fetchData}>Fetch Data</Button>
+      </div>
+    );
+  }
+}
+
+export default Admin;
