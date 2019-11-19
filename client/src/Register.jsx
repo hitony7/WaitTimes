45cd88873497c6008f83ba5b7629197bca618618
Russell McWhae <russell.mcwhae@gmail.com>
2019-11-19 09:36:11 -0700

    Adds unused register page. To be implemented later

---
+++ client/src/Register.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Form, Icon, Input, Button } from 'antd';
+import { Redirect } from "react-router-dom";
+// import $ from 'jquery';
+import axios from 'axios';
+import NOT_LOGGED_IN from './App.js';
+
+class Register extends Component {
+
+  state = {
+    redirect: false,
+    message: 'Welcome to the new user registration page.',
+  }
+
+
+  // componentDidUpdate() {
+  //   if (this.state.redirect) {
+  //     this.setState({ redirect: false })
+  //   }
+  // }
+
+
+  render() {
+    // const { getFieldDecorator } = this.props.form;
+    if (this.state.redirect) {
+      return (<Redirect to='/' />)
+    }
+    return (
+      <div>
+        <h1>Register</h1>
+        <h2>{this.state.message}</h2>
+      </div>
+    );
+  }
+}
+
+export default Register;
