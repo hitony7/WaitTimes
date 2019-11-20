5f70c111972c0b37c6398ef06d0693710665862e
Russell McWhae <russell.mcwhae@gmail.com>
2019-11-20 12:20:44 -0700

    Event now shows (ugly) list of questions

--- client/src/Event.jsx
+++ client/src/Event.jsx
@@ -11,17 +11,49 @@ const { TextArea } = Input;
 
 class Event extends Component {
 
-  state = {
-    redirect: false,
-    message: 'Please provide some details for your upcoming ER visit. We can also add hospital selection on this page (stretch)',
-    serverResponse: '',
-    serverResponseErrors: null,
-    triageQuestions: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+      message: 'Please provide some details for your upcoming ER visit.',
+      serverResponse: '',
+      serverResponseErrors: null,
+      triageQuestionsError: null,
+      triageQuestionsAreLoaded: false,
+      triageQuestions: []
+    };
   }
 
   cancel = e => {
     e.preventDefault();
     this.setState({ redirect: true });
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: { 'Authorization': "Bearer " + token }
+    };
+    axios
+      .get('/api/triage_questions', config) // You can simply make your requests to "/api/whatever you want"
+      .then(response => {
+        // handle success
+        console.log(response.data); // The entire response from the Rails API
+        // console.log(response.data.message); // Just the message
+        this.setState({
+          triageQuestionsAreLoaded: true,
+          triageQuestions: response.data
+        });
+      },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            triageQuestionsAreLoaded: true,
+            triageQuestionsError: error
+          });
+        });
   }
 
 
@@ -99,58 +131,74 @@ class Event extends Component {
 
   render() {
     const { getFieldDecorator } = this.props.form;
+    const { triageQuestionsError, triageQuestionsAreLoaded, triageQuestions } = this.state;
     if (this.state.redirect) {
-      return (<Redirect to='/questions' />)
+      return (<Redirect to='/patient' />)
     }
     if (this.state.loggedInStatus === NOT_LOGGED_IN) {
       return (<Redirect to='/' />)
     }
-    return (
-      <div className="sign-in">
-        <Alert
-          message="Warning"
-          description="If this is a real emergency, close this app and dial 911!"
-          type="error"
-          closable
-        />
-        <h1>New ER Visit for {this.props.patient_name}</h1>
-        <h2>{this.state.message}</h2>
+    if (triageQuestionsError) {
+      return <div>Error: {triageQuestionsError.message}</div>
+    }
+    else if (!triageQuestionsAreLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="sign-in">
+          <Alert
+            message="Warning"
+            description="If this is a real emergency, close this app and dial 911!"
+            type="error"
+            closable
+          />
+          <h1>New ER Visit for {this.props.patient_name}</h1>
+          <h2>{this.state.message}</h2>
 
-        <Form onSubmit={this.startEmergencyEvent} className="registration-form">
-          <Form.Item>
-            {getFieldDecorator('visit_description', {
-              rules: [{ required: true, message: 'Please input a description!' }],
-            })(
-              <TextArea
-                rows={5}
-                name="visit_description"
-                id="visit_description"
-                type="text"
-                placeholder="Give a quick summary of your emergency."
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button htmlType="submit">
-              Submit
+          <ul>
+            {triageQuestions.map(question => (
+              <li key={question.id}>
+                {question.question_text}
+              </li>
+            ))}
+          </ul>
+
+          <Form onSubmit={this.startEmergencyEvent} className="registration-form">
+            <Form.Item>
+              {getFieldDecorator('visit_description', {
+                rules: [{ required: true, message: 'Please input a description!' }],
+              })(
+                <TextArea
+                  rows={5}
+                  name="visit_description"
+                  id="visit_description"
+                  type="text"
+                  placeholder="Give a quick summary of your emergency."
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType="submit">
+                Submit
           </Button>
-          </Form.Item>
-          {this.state.serverResponse && <Alert
-            message="Event Initiation Succesful"
-            description={this.state.serverResponse}
-            type="success"
-            showIcon
-          />}
-          {this.state.serverResponseErrors && <List
-            header={<h2>Error!</h2>}
-            bordered
-            dataSource={this.state.serverResponseErrors}
-            renderItem={item => <List.Item>{item}</List.Item>}
-          />}
-        </Form>
-
-      </div>
-    );
+            </Form.Item>
+            {this.state.serverResponse && <Alert
+              message="Event Initiation Succesful"
+              description={this.state.serverResponse}
+              type="success"
+              showIcon
+            />}
+            {this.state.serverResponseErrors && <List
+              header={<h2>Error!</h2>}
+              bordered
+              dataSource={this.state.serverResponseErrors}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />}
+          </Form>
+          <Button onClick={this.cancel}>Cancel</Button>
+        </div>
+      );
+    }
   }
 }
 
