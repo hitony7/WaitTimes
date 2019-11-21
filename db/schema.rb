5992e87267d66081453368baa4bd7ae30ad26e38
Russell McWhae <russell.mcwhae@gmail.com>
2019-11-20 17:59:38 -0700

    DB migration that adds boolean is_active field to er visits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_021236) do
+ActiveRecord::Schema.define(version: 2019_11_21_005139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_021236) do
     t.datetime "updated_at", null: false
     t.bigint "users_id"
     t.bigint "emergency_rooms_id"
+    t.boolean "is_active", default: true
     t.index ["emergency_rooms_id"], name: "index_emergency_room_visits_on_emergency_rooms_id"
     t.index ["users_id"], name: "index_emergency_room_visits_on_users_id"
   end
